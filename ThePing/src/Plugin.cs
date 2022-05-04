f682a4b7d55cc61c9db49b139a4d89dd9bebb226
haohlin <haohan.lin@outlook.com>
2022-05-04 16:20:24 +0800

    Add volume curve to The Ping

--- ThePing/src/Plugin.cs
+++ ThePing/src/Plugin.cs
@@ -7,21 +7,35 @@ using FistVR;
 
 namespace ThePing
 {
+
     [BepInProcess("h3vr.exe")]
-    [BepInPlugin("HLin-ThePing", PluginInfo.PLUGIN_NAME, "1.0.0")]
+    [BepInPlugin("HLin-ThePing", PluginInfo.PLUGIN_NAME, "1.0.2")]
     public class Plugin : BaseUnityPlugin
     {
+        public static AnimationCurve customRolloffCurve;
         private void Awake()
         {
             Harmony harmony = new Harmony("HLin-ThePing");
             MethodInfo original = AccessTools.Method(typeof(FVRPooledAudioSource), "Play");
             MethodInfo patch = AccessTools.Method(typeof(Plugin), "Play_MyPatch");
             harmony.Patch(original, new HarmonyMethod(patch));
+            customRolloffCurve = new AnimationCurve();
+            customRolloffCurve = new AnimationCurve();
+            customRolloffCurve.AddKey(new Keyframe(0, 1f));
+            customRolloffCurve.AddKey(new Keyframe(50, 0.5f));
+            customRolloffCurve.AddKey(new Keyframe(100, 0.3f));
+            customRolloffCurve.AddKey(new Keyframe(1000f, 0.125f));
+            customRolloffCurve.AddKey(new Keyframe(2500f, 0.1f));
+            customRolloffCurve.SmoothTangents(1, -0.9f);
+            customRolloffCurve.SmoothTangents(2, -0.9f);
+            customRolloffCurve.SmoothTangents(3, -0.9f);
         }
 
         public static void Play_MyPatch(FVRPooledAudioSource __instance, AudioEvent audioEvent, Vector3 pos, Vector2 pitch, Vector2 volume, AudioMixerGroup mixerOverride = null)
         {
-            __instance.Source.maxDistance = 2000f;
+            __instance.Source.maxDistance = 2500f;
+            __instance.Source.rolloffMode = AudioRolloffMode.Custom;
+            __instance.Source.SetCustomCurve(AudioSourceCurveType.CustomRolloff, customRolloffCurve);
         }
     }
 }
