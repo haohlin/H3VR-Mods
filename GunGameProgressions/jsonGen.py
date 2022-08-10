827aa138b67e3b098463c81fd2d3d0419fbd3381
haohlin <haohan.lin@outlook.com>
2022-08-10 09:55:45 +0800

    Added more blacklisted firearms and cartridges & bug fix

--- GunGameProgressions/jsonGen.py
+++ GunGameProgressions/jsonGen.py
@@ -1,12 +1,16 @@
 import json
 import random
 
+enemyList = ["RW_Lemonhead","M_MercWiener_Scout","M_Swat_Riflewiener","M_Swat_Scout","RW_Beefkicker","RW_Pig","RW_Rot"]
+
+enemyType = enemyList[6]
+
 # Data to be written
 dictionary = {
-    "Name": "All in One",
+    "Name": "All in One (" + enemyType + ")",
     "Description": "The entire H3VR arsenal",
-    "OrderType": 2,
-    "EnemyType": "M_Swat_Scout", #"M_MercWiener_Shield",
+    "OrderType": 1,
+    "EnemyType": enemyType, #"M_MercWiener_Shield",
     "Guns": [],
     "GunNames": [],
     "MagNames": [],
@@ -16,11 +20,12 @@ dictionary = {
 BlackList = ["MF_Syringegun","Degle","PotatoGun","Stinger","COOLCLOSEDBOLT","COOLREVOLVER","GrappleGun",
             "GravitonBeamer","PlungerLauncher","BrownBess","BrownBessRamrod","HeavyFlintlock18thCentury",
             "HeavyFlintlock18thCenturyRamrod", "SustenanceCrossbow", "MF_Flamethrower", "MF_Medical180", 
-            "MF_LongShot", "Pocket1906", "PocketHammer1903","OTS38", "Jackhammer", "Flaregun","Saiga12k",
-            "JunkyardFlameThrower","M72A7","M320GrenadeLauncher", "M224Mortar"]
+            "MF_LongShot", "Pocket1906", "PocketHammer1903","OTS38", "Jackhammer", "Flaregun",#"LadiesPepperbox",
+            "JunkyardFlameThrower","M72A7","M320GrenadeLauncher", "M224Mortar","SP5K","SP5KA2","SP5KA3",
+            "SP5KFolding","Whizzbanger","P6Twelve"]
 mag_BL =    ["MagazineMp515rnd","MagazineAK74_10rnd","MagazineStanag10rnd","MagazineStanag5rnd","MagazineVZ58_10Rnd",
             "MagazineMp515rndStraight"]
-cartridge_BL = ["12GaugeShellFreedomfetti","12GaugeShellRedFlare","23x75mmR_Flash","20GaugeShellFreedomfetti"]
+cartridge_BL = ["12GaugeShellCannonball","12GaugeShellFreedomfetti","12GaugeShellRedFlare","23x75mmR_Flash","20GaugeShellFreedomfetti","Cartridge_40mmCaseless_TPM", "Cartridge_16Gauge_Freedomfetti"]
 clip_BL =   []
 
 # Opening JSON file
@@ -45,7 +50,6 @@ for obj in json_object:
 
 for obj in json_object:
     if obj['Category'] == "Firearm" and not obj["IsModContent"] and not obj["ObjectID"] in BlackList:
-        dictionary["GunNames"].append(obj["ObjectID"])
         if obj["MagazineType"] != 0:
             random.shuffle(mag_list)
             dictionary["CategoryIDs"].append(0)
@@ -60,13 +64,18 @@ for obj in json_object:
                 if clip_obj["ClipType"] == obj["ClipType"]:
                     dictionary["MagNames"].append(clip_obj['ObjectID'])
                     break
-        else:
+        elif obj["RoundType"] != 0:
             random.shuffle(cartridge_list)
             dictionary["CategoryIDs"].append(2)
             for cartridge_obj in cartridge_list:
                 if cartridge_obj["RoundType"] == obj["RoundType"]:
                     dictionary["MagNames"].append(cartridge_obj['ObjectID'])
                     break
+        else:
+            print(obj["ObjectID"] + " has 000 mag type")
+            dictionary["CategoryIDs"].append(2)
+            dictionary["MagNames"].append("22LRCartridgeTracer")
+        dictionary["GunNames"].append(obj["ObjectID"])
 
 # dictionary["GunNames"] = dictionary["GunNames"][344:]
 # dictionary["MagNames"] = dictionary["MagNames"][344:]
@@ -83,5 +92,6 @@ print("Cartridge weapon count", dictionary["CategoryIDs"].count(2))
 json_object = json.dumps(dictionary, indent=4)
  
 # Writing to sample.json
-with open("GunGameWeaponPool_All in One.json", "w") as outfile:
+json_name = "GunGameWeaponPool_All in One (" + enemyType + ").json"
+with open(json_name, "w") as outfile:
     outfile.write(json_object)
