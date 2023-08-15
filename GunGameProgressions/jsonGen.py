26b02de61d416050370fcd2152aa74775a648a87
haohlin <haohan.lin@outlook.com>
2023-08-15 10:55:03 +0800

    add Thunderstore package and gungame script update

--- GunGameProgressions/jsonGen.py
+++ GunGameProgressions/jsonGen.py
@@ -1,16 +1,21 @@
 import json
 import random
+import argparse
 
-enemyList = ["RW_Lemonhead","M_MercWiener_Scout","M_Swat_Riflewiener","M_Swat_Scout","RW_Beefkicker","RW_Pig","RW_Rot"]
+parser = argparse.ArgumentParser(description="Select Sosig type")
+parser.add_argument("sosigType", type=int, default=0)
+args = parser.parse_args()
 
-enemyType = enemyList[6]
+enemyList = ["RW_Lemonhead","M_MercWiener_Scout","M_Swat_Riflewiener","RW_Beefkicker","RW_Pig","RW_Rot"]
+
+enemyType = enemyList[args.sosigType]
 
 # Data to be written
 dictionary = {
     "Name": "All in One (" + enemyType + ")",
     "Description": "The entire H3VR arsenal",
     "OrderType": 1,
-    "EnemyType": enemyType, #"M_MercWiener_Shield",
+    "EnemyType": enemyType,
     "Guns": [],
     "GunNames": [],
     "MagNames": [],
@@ -20,12 +25,20 @@ dictionary = {
 BlackList = ["MF_Syringegun","Degle","PotatoGun","Stinger","COOLCLOSEDBOLT","COOLREVOLVER","GrappleGun",
             "GravitonBeamer","PlungerLauncher","BrownBess","BrownBessRamrod","HeavyFlintlock18thCentury",
             "HeavyFlintlock18thCenturyRamrod", "SustenanceCrossbow", "MF_Flamethrower", "MF_Medical180", 
-            "MF_LongShot", "Pocket1906", "PocketHammer1903","OTS38", "Jackhammer", "Flaregun",#"LadiesPepperbox",
+            "MF_LongShot", "Pocket1906", "PocketHammer1903","OTS38", "Jackhammer", "Flaregun",
             "JunkyardFlameThrower","M72A7","M320GrenadeLauncher", "M224Mortar","SP5K","SP5KA2","SP5KA3",
-            "SP5KFolding","Whizzbanger","P6Twelve"]
-mag_BL =    ["MagazineMp515rnd","MagazineAK74_10rnd","MagazineStanag10rnd","MagazineStanag5rnd","MagazineVZ58_10Rnd",
-            "MagazineMp515rndStraight"]
-cartridge_BL = ["12GaugeShellCannonball","12GaugeShellFreedomfetti","12GaugeShellRedFlare","23x75mmR_Flash","20GaugeShellFreedomfetti","Cartridge_40mmCaseless_TPM", "Cartridge_16Gauge_Freedomfetti"]
+            "Whizzbanger","P6Twelve","MF_Signaler","MP5SFA2","MP5SD1","MP5SD2","MP5SD3","MP5SD5",
+            "MP5SD4","MP5K","MP5KA2","MP5KA3","MP510A4","MP540A4","MP5A2","MP5A3","MP5A4","MP5KN",]
+mag_BL =    ["MagazineMp515rnd","MagazineAK74_10rnd","MagazineStanag10rnd","MagazineAKMTactical10rnd",
+            "MagazineStanag5rnd","MagazineVZ58_10Rnd","MagazineMp515rndStraight", "MagazineMini145rnd",
+            "MagazineMini1410rnd","MagazineVSSVintorez10rnd","MagazineEvo315rnd","MagazineModel38_10rnd"]
+cartridge_BL =  ["12GaugeShellCannonball","12GaugeShellFreedomfetti","12GaugeShellRedFlare","23x75mmR_Flash",
+                "20GaugeShellFreedomfetti","Cartridge_40mmCaseless_TPM", "Cartridge_16Gauge_Freedomfetti",
+                "Cartridge50mmFlareClassic", "Cartridge50mmFlareConflagration","Cartridge50mmFlareDangerClose",
+                "Cartridge50mmFlareSunburn","27x90mmCartridgeSmokescreen","27x90mmCartridgeTriFlash",
+                "Cartridge366UltraMagnumSalute","Cartridge366UltraMagnumRetort", "18x50mmPackawhallop_Gobsmacka",
+                "Cartridge_40mmCaseless_SMK","Cartridge_40mmCaseless_SF1","Cartridge366UltraMagnumDebuff",
+                "Cartridge_40x46Grenade_M651","23x75mmR_CSGas","Cartridge_40x46Grenade_M781","Cartridge_40x46Grenade_X1776"]
 clip_BL =   []
 
 # Opening JSON file
@@ -48,8 +61,10 @@ for obj in json_object:
     if obj['Category'] == "Cartridge" and not obj["IsModContent"] and not obj["ObjectID"] in cartridge_BL:
         cartridge_list.append(obj)
 
+
 for obj in json_object:
     if obj['Category'] == "Firearm" and not obj["IsModContent"] and not obj["ObjectID"] in BlackList:
+        MagNameFound = 0
         if obj["MagazineType"] != 0:
             random.shuffle(mag_list)
             dictionary["CategoryIDs"].append(0)
@@ -77,9 +92,6 @@ for obj in json_object:
             dictionary["MagNames"].append("22LRCartridgeTracer")
         dictionary["GunNames"].append(obj["ObjectID"])
 
-# dictionary["GunNames"] = dictionary["GunNames"][344:]
-# dictionary["MagNames"] = dictionary["MagNames"][344:]
-# dictionary["CategoryIDs"] = dictionary["CategoryIDs"][344:]
 print(len(dictionary["GunNames"]))
 print(len(dictionary["MagNames"]))
 print(len(dictionary["CategoryIDs"]))
---
+++ RemoveWhiteOut/Thunderstore/HLin_Mods-No_WhiteOut/README.md
@@ -0,0 +1,5 @@
+# No WhiteOut
+
+***Now also works in TNH*** 
+Removes the high altitude whiteout effect in Winter Wasteland. Now you can snipe those mines from the TIPPITY TOP!
+
