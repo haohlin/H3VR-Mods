467b42dab61a87b856f815db2f55045c64d5c5ad
haohlin <haohan.lin@outlook.com>
2024-03-02 15:10:30 +0800

    Add some new mods, updated gungame_progression and better_sosig_spawner

---
+++ BetterGasTank/src/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace BetterGasTank
+{
+    [BepInProcess("h3vr.exe")]
+    [BepInPlugin("HLin-BetterGasTank", MyPluginInfo.PLUGIN_NAME, "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static readonly Harmony harmony = new Harmony("HLin-BetterGasTank");
+
+        public void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo("Loaded BetterGasTank Successfully!");
+        }
+    }
+}
